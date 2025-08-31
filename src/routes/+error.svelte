837c652270d4bfca19a6e52a076cb34caa63d5c0
MatthiasBenaets <matthias.benaets@gmail.com>
2025-08-31 17:35:59 +0200

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<div class="my-auto flex h-screen w-full flex-col items-center justify-center">
+	<h1>{page.status} {page.error?.message}</h1>
+	<a href="/" class="rounded-md border border-neutral-700 p-2 hover:bg-neutral-800">Return Home</a>
+</div>
